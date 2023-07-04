965fa02e35e4a22987d8c1d892106e50f7ce80c0
AnurupDey17 <88165422+AnurupDey17@users.noreply.github.com>
2023-07-04 15:16:55 +0530

    Create Jenkinsfile.py
    Trying 1st python program to run on jenkins

---
+++ Jenkinsfile.py
@@ -0,0 +1,12 @@
+import os
+l1=[3,6,9,19,23,54,12]
+l1.reverse()
+print('Reverse list is',l1)
+
+l2=[34,67,31,76,93,35]
+l2.sort()
+print('Sortedlist is',l2)
+
+l3=list()
+l3=l1[2:5].copy()
+print('3rd list is',l3)
